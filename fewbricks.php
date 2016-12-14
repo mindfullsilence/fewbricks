5c1cbba6f4c0f5f88cd7aab4dd64654ea045d9fd
Timothy Anderson <mindfullsilence@gmail.com>
2016-12-13 23:01:03 -0700

    cleaning

--- fewbricks.php
+++ fewbricks.php
@@ -13,33 +13,9 @@ License: GPLv3
 */
 
 // Exit if accessed directly
-if (!defined('ABSPATH')) {
-    exit;
+if ( file_exists( $composer_autoload = __DIR__ . '/vendor/autoload.php' ) /* check in self */
+	|| file_exists( $composer_autoload = $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php') /* check in root directory */
+) {
+	require_once $composer_autoload;
+  fewbricks\fewbricks::construct();
 }
-
-$fewbricks_lib_path = plugin_dir_path(__FILE__) . 'lib/';
-
-require_once($fewbricks_lib_path . 'fewbricks.php');
-
-fewbricks\fewbricks::construct();
-
-/**
- * Update related stuff
- */
-require_once($fewbricks_lib_path . 'wp-autoupdate.php');
-
-add_action('init', function() {
-
-    // set auto-update params
-    $plugin_current_version = '1.4.0';
-    $plugin_remote_path = 'http://fewbricks.folbert.com/update/update.php';
-    $plugin_slug = plugin_basename(__FILE__);
-    $license_user = 'null';
-    $license_key = 'null';
-
-    // only perform Auto-Update call if a license_user and license_key is given
-    if ($license_user && $license_key && $plugin_remote_path) {
-        new wp_autoupdate ($plugin_current_version, $plugin_remote_path, $plugin_slug, $license_user, $license_key);
-    }
-
-});
